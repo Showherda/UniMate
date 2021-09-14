70fe5e9503390c3bd18d6d50e413d7fc6ff0ad25
Showherda <showherda@gmail.com>
2021-09-14 14:57:04 +0600

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -1,20 +1,23 @@
-let requestURL='https://showherda.github.io/unimate/sdata.json';
+let requestURL='https://showherda.github.io/unimate/sdata.txt';
 $(document).ready(() => {
 	let request=new XMLHttpRequest();
 	request.open('GET', requestURL);
-	request.responseType='json';
+	request.responseType='txt';
 	request.send();
-	var unis;
+	var unis=[];
 	request.onload=() => {
-		unis=request.response;
+		let tmp=request.response.split(';');
+		for (let v of tmp)
+			unis.push(Array.from(v.split(',')));
+		console.log(unis);
 		$('table').DataTable({
 			data:unis,
-			'columns':[
-				{'title':'Institution'},
-				{'title':'ED to RD Acceptance Ratio'},
-				{'title':'Percentage of International Noncitizens to All Undergraduates'},
-				{'title':'Percentage of International Noncitizens Receiving Aid'},
-				{'title':'Estimated Family Contribution'}
+			columns:[
+				{title:'Institution'},
+				{title:'ED to RD Acceptance Ratio'},
+				{title:'Percentage of International Noncitizens to All Undergraduates'},
+				{title:'Percentage of International Noncitizens Receiving Aid'},
+				{title:'Estimated Family Contribution'}
 			]
 		});
 	}
