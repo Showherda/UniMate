ccda5f2d3b93808b0d000d1f941ae0d0538e81cc
Showherda <showherda@gmail.com>
2021-09-16 13:46:17 +0600

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -1,16 +1,46 @@
-let requestURL='https://showherda.github.io/unimate/sdata.txt';
+const requestURL='https://showherda.github.io/unimate/data.txt';
 $(document).ready(() => {
 	let request=new XMLHttpRequest();
 	request.open('GET', requestURL);
 	request.responseType='txt';
 	request.send();
-	var unis=[];
+	var all=[[]];
 	request.onload=() => {
-		let tmp=request.response.split(';');
-		for (let v of tmp)
-			unis.push(Array.from(v.replace('\n','').split(',')));
+		document.querySelector('button').disabled=false;
+		for (let v of request.response.split(';')){
+			v=v.replace('\n', '');
+			let t=[], i=0, s='';
+			while (i<v.length){
+				if (v[i]===',')
+					all[all.length-1].push(s),
+					s='';
+				else if (v[i]==='"'){
+					let j=i+1;
+					while (v[j]!=='"')
+						s+=v[j++];
+					t.push(s);
+					i=j
+				}
+				else
+					s+=v[i];
+				i+=1
+			}
+			all.push([]);
+		}
+		all.pop();
+	};
+	var form=document.querySelector('form');
+	var formData=new FormData(form);
+	form.addEventListener('submit', (e) => {
+		e.preventDefault();
+		var res=[], efc=formData.get('efc'), ed=formData.get('ed');
+		console.log(efc, ed);
+		for (let v of all)
+			if (efc<=v[15] && ((ed && v[1]>0) || !ed))
+				res.push(v);
+		console.log(res);
 		$('table').DataTable({
-			data:unis,
+			data:res,
 			columns:[
 				{title:'Institution'},
 				{title:'ED to RD Acceptance Ratio'},
@@ -19,5 +49,5 @@ $(document).ready(() => {
 				{title:'Estimated Family Contribution'}
 			]
 		});
-	}
-})
+	});
+});
