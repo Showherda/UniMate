a61e1b291c4dc4ee396e0de066752d399da36d8e
Showherda <showherda@gmail.com>
2021-09-18 15:22:14 +0600

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -5,6 +5,10 @@ $(document).ready(() => {
 	request.responseType='txt';
 	request.send();
 	var all=[[]];
+	var form=document.querySelector('form');
+	var formData=new FormData(form);
+	var res=[[]];
+	var table=$('table');
 	request.onload=() => {
 		document.querySelector('button').disabled=false;
 		for (let v of request.response.split(';')){
@@ -29,12 +33,28 @@ $(document).ready(() => {
 			all.push([]);
 		}
 		all.pop();
+		for (let v of all){
+			for (let i=0; i<v.length; i++)
+				if (i==0 || i==4 || i==9 || i==14 || i==15)
+					res[res.length-1].push(v[i]);
+			res.push([]);
+		}
+		res.pop();
+		table.DataTable({
+			data:res,
+			columns:[
+				{title:'Institution'},
+				{title:'ED to RD Acceptance Ratio'},
+				{title:'Percentage of International Noncitizens to All Undergraduates'},
+				{title:'Percentage of International Noncitizens Receiving Aid'},
+				{title:'Estimated Family Contribution'}
+			]
+		});
 	};
-	var form=document.querySelector('form');
-	var formData=new FormData(form);
 	form.addEventListener('submit', (e) => {
 		e.preventDefault();
-		var res=[[]], efc=formData.get('efc'), ed=formData.get('ed');
+		var efc=formData.get('efc'), ed=formData.get('ed');
+		res=[[]];
 		for (let v of all)
 			if (parseInt(v[v.length-1])<=parseInt(efc) && ((ed==='on' && parseFloat(v[1])>0) || ed==='off')){
 				for (let i=0; i<v.length; i++)
@@ -43,7 +63,9 @@ $(document).ready(() => {
 				res.push([]);
 			}
 		res.pop();
-		$('table').DataTable({
+		console.log(res);
+		table.DataTable().clear().destroy();
+		table.DataTable({
 			data:res,
 			columns:[
 				{title:'Institution'},
